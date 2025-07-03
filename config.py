8e05fc3bccd9e134057b55dd17b3902d09b8a1f3
Nilsonfts <nil.market58@gmail.com>
2025-07-04 00:49:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
+# config.py
+import os
 
+# Токен вашего бота
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# ID вашего телеграм-канала
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+
+# --- НОВЫЕ ПЕРЕМЕННЫЕ ДЛЯ GOOGLE SHEETS ---
+GOOGLE_SHEET_KEY = "1bp7NwfWe1MCb7S6wkaQtaxJEIDaFcFPwv9V_kzlchXs"
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
+
+# Проверки, что переменные загружены
+if not BOT_TOKEN:
+    raise ValueError("Не найдена переменная окружения BOT_TOKEN!")
+if not CHANNEL_ID:
+    raise ValueError("Не найдена переменная окружения CHANNEL_ID!")
+if not GOOGLE_CREDENTIALS_JSON:
+    raise ValueError("Не найдена переменная окружения GOOGLE_CREDENTIALS_JSON!")
