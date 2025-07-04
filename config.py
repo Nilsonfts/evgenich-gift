9f668587abe22bd07730671906c076d2e417204b
Nilsonfts <nil.market58@gmail.com>
2025-07-04 07:18:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -15,12 +15,15 @@ GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
 HELLO_STICKER_ID = os.getenv("HELLO_STICKER_ID")
 NASTOYKA_STICKER_ID = os.getenv("NASTOYKA_STICKER_ID")
 THANK_YOU_STICKER_ID = os.getenv("THANK_YOU_STICKER_ID")
-# Если хочешь отдельный стикер для бонуса за друга, добавь его ID в эту переменную на Railway
 FRIEND_BONUS_STICKER_ID = os.getenv("FRIEND_BONUS_STICKER_ID") 
+
+# --- Ссылки ---
+MENU_URL = os.getenv("MENU_URL")
 
 # --- Проверки ---
 if not all([
     BOT_TOKEN, CHANNEL_ID, GOOGLE_SHEET_KEY, GOOGLE_CREDENTIALS_JSON, 
-    HELLO_STICKER_ID, NASTOYKA_STICKER_ID, THANK_YOU_STICKER_ID, ADMIN_IDS, REPORT_CHAT_ID
+    HELLO_STICKER_ID, NASTOYKA_STICKER_ID, THANK_YOU_STICKER_ID, ADMIN_IDS, 
+    REPORT_CHAT_ID, MENU_URL
 ]):
-    raise ValueError("Одна или несколько переменных окружения не установлены! Проверь все, включая BOSS_ID и REPORT_CHAT_ID.")
+    raise ValueError("Одна или несколько переменных окружения не установлены! Проверь все.")
