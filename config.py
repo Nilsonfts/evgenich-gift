33d56698f391e91191cd3d4878cab6c61242466d
Nilsonfts <nil.market58@gmail.com>
2025-07-04 03:37:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,7 @@ GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
 # --- Стикеры ---
 HELLO_STICKER_ID = os.getenv("HELLO_STICKER_ID")
 NASTOYKA_STICKER_ID = os.getenv("NASTOYKA_STICKER_ID")
+THANK_YOU_STICKER_ID = os.getenv("THANK_YOU_STICKER_ID") # Новая переменная
 
 # --- Проверки ---
 if not all([
@@ -20,6 +21,7 @@ if not all([
     GOOGLE_SHEET_KEY, 
     GOOGLE_CREDENTIALS_JSON, 
     HELLO_STICKER_ID, 
-    NASTOYKA_STICKER_ID
+    NASTOYKA_STICKER_ID,
+    THANK_YOU_STICKER_ID # Новая переменная в проверке
 ]):
-    raise ValueError("Одна или несколько переменных окружения не установлены! Проверь все, включая ID стикеров.")
+    raise ValueError("Одна или несколько переменных окружения не установлены! Проверь все, включая все ID стикеров.")
