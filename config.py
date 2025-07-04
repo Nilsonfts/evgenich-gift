38a30ad0e34b07fba0d5ab6fddd6a2ba9f8abb12
Nilsonfts <nil.market58@gmail.com>
2025-07-04 03:31:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,17 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 GOOGLE_SHEET_KEY = os.getenv("GOOGLE_SHEET_KEY")
 GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
 
+# --- Стикеры ---
+HELLO_STICKER_ID = os.getenv("HELLO_STICKER_ID")
+NASTOYKA_STICKER_ID = os.getenv("NASTOYKA_STICKER_ID")
+
 # --- Проверки ---
-if not all([BOT_TOKEN, CHANNEL_ID, GOOGLE_SHEET_KEY, GOOGLE_CREDENTIALS_JSON]):
-    raise ValueError("Одна или несколько переменных окружения не установлены!")
+if not all([
+    BOT_TOKEN, 
+    CHANNEL_ID, 
+    GOOGLE_SHEET_KEY, 
+    GOOGLE_CREDENTIALS_JSON, 
+    HELLO_STICKER_ID, 
+    NASTOYKA_STICKER_ID
+]):
+    raise ValueError("Одна или несколько переменных окружения не установлены! Проверь все, включая ID стикеров.")
